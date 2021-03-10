b4a6281e41b5f78d4fbd840ac40a5bb724e3c694
Salimboevm <salimboyevm@gmail.com>
2021-03-11 00:31:39 +0500

    Door opening test working

---
+++ Source/EscapeRoomPuzzle/OpenDoor.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	initialYaw = GetOwner()->GetActorRotation().Yaw;
+	currentYaw = initialYaw;
+	targetYaw += initialYaw;
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	currentYaw = FMath::Lerp(currentYaw, targetYaw, 0.02f);
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = currentYaw;
+	GetOwner()->SetActorRotation(DoorRotation);
+
+	
+	//FRotator OpenDoor(0.f, targetYaw, 0.f);
+	//OpenDoor.Yaw = FMath::FInterpConstantTo(currentYaw, targetYaw, DeltaTime, 30);
+	//GetOwner()->SetActorRotation(OpenDoor);
+	// ...
+}
+
